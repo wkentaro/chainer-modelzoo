f4cf2b939dd54e10039c06e47572cbfe37d28d2a
Kentaro Wada <www.kentaro.wada@gmail.com>
2016-12-16 04:10:29 +0900

    Improve _lib.py download function

--- _lib.py
+++ _lib.py
@@ -18,12 +18,16 @@ def download(url, path, quiet=False):
 def cached_download(url, path, md5=None, quiet=False):
 
     def check_md5(path, md5, quiet=False):
+        if md5 and len(md5) != 32:
+            raise ValueError('md5 must be 32 charactors.\n'
+                             'actual: {} ({})'.format(md5, len(md5)))
         if not quiet:
             print('Checking md5 of file: {}'.format(path))
         is_same = hashlib.md5(open(path, 'rb').read()).hexdigest() == md5
         return is_same
 
     if osp.exists(path) and not md5:
+        print('{} already exists'.format(path))
         return path
     elif osp.exists(path) and md5 and check_md5(path, md5):
         return path
